860e1555297b61bea8eb328ef596b91f2e96bee6
Lincoln <jeanlincol@gmail.com>
2023-06-19 15:06:35 -0300

    blade @if @else

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,11 +1,9 @@
 <h3>Fornecedor</h3>
 
-{{-- Fica o comentario que sera descartado! --}}
-
-@php
-    // para comentario de uma linha
-    
-    /*
-        para comentarios de varias linhas
-    */
-@endphp
+@if (count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados </h3>
+@elseif (count($fornecedores) > 10)
+    <h3>Existem varios fornecedores cadastrados</h3>
+@else
+    <h3>Ainda nao existem fornecedores cadastrados</h3>
+@endif
