74c6446e537332f66337432e51d51ba4781dc5d7
Lincoln <jeanlincol@gmail.com>
2023-06-19 15:11:58 -0300

    Blade - @unless

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,9 +1,18 @@
 <h3>Fornecedor</h3>
 
-@if (count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados </h3>
-@elseif (count($fornecedores) > 10)
-    <h3>Existem varios fornecedores cadastrados</h3>
-@else
-    <h3>Ainda nao existem fornecedores cadastrados</h3>
+{{-- enquanto o if executa se o retorno for true, o unless executa se for false --}}
+
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Status: {{ $fornecedores[0]['status'] }}
+<br>
+
+@if (!($fornecedores[0]['status'] == 'S'))
+    Fornecedor inativo
 @endif
+
+<br>
+
+@unless ($fornecedores[0]['status'] == 'S')
+    Fornecedor inativo
+@endunless
