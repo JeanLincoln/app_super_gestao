1ecd1d84d518c50b737f25b98248acf46f9c486c
Lincoln <jeanlincol@gmail.com>
2023-06-19 13:34:41 -0300

    Nomeando rotas

--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal');
-Route::get('/sobrenos', 'SobrenosController@principal');
-Route::get('/contato', 'ContatoController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobrenos', 'SobrenosController@principal')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@principal')->name('site.contato');
 Route::get('/login', function () {
     return 'login';
-});
+})->name('site.login');
 
 Route::prefix('/app')->group(function () {
     Route::get('/clientes', function () {
         return 'clientes';
-    });
+    })->name('app.clientes');
     Route::get('/fornecedores', function () {
         return 'fornecedores';
-    });
+    })->name('app.fornecedores');
     Route::get('/produtos', function () {
         return 'produtos';
-    });
+    })->name('app.produtos');
 });
