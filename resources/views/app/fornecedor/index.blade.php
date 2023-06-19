5fea97430e84354ef5014c4a96edb3b057eab0c7
Lincoln <jeanlincol@gmail.com>
2023-06-19 14:53:36 -0300

    Incluindo comentarios e blocos de php puro

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,11 @@
+<h3>Fornecedor</h3>
+
+{{-- Fica o comentario que sera descartado! --}}
+
+@php
+    // para comentario de uma linha
+    
+    /*
+        para comentarios de varias linhas
+    */
+@endphp
