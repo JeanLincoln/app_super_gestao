91790e50613f5b5511967993a2ab7c56512ddeaa
Lincoln <jeanlincol@gmail.com>
2023-06-19 13:24:23 -0300

    criando menu de navegacao

--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,1 +1,6 @@
 <h3>Sobre-nos (view) </h3>
+<ul>
+    <li><a href="/">Principal</a></li>
+    <li><a href="/sobrenos">Sobre-nós</a></li>
+    <li><a href="/contato">Contato</a></li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,3 @@ Route::get('/', 'PrincipalController@principal');
 Route::get('/sobrenos', 'SobrenosController@principal');
 
 Route::get('/contato', 'ContatoController@principal');
-
-Route::get(
-    '/contato/{nome}/{categoria_id?}',
-    function (string $nome = 'Desconhecido', int $categoria_id = 1) {
-        echo 'Estamos aqui:' . $nome . ' - ' . $categoria_id;
-    }
-    // o primeiro parametro e a variavel e o segundo a expressao regular
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
