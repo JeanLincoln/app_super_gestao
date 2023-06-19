7f9b85fc5e22ca85abaa4ff2633f1b3c57f1f649
Lincoln <jeanlincol@gmail.com>
2023-06-19 15:41:46 -0300

    Blade - @switch/case

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -16,14 +16,28 @@
 --}}
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    Fornecedor: {{ $fornecedores[0]['nome'] ?? 'Dado nao foi preenchido' }}
     <br>
-    Status: {{ $fornecedores[0]['status'] }}
+    Status: {{ $fornecedores[0]['status'] ?? 'Dado nao foi preenchido' }}
     <br>
     CPNJ : {{ $fornecedores[0]['cnpj'] ?? 'Dado nao foi preenchido' }}
-    {{--
-        Se a variavel testada nao estiver definida (isset)
-    ou
-        $variavel testado possuir o valor null
-    --}}
+    <br>
+    Telefone : ({{ $fornecedores[0]['ddd'] ?? 'Dado nao foi preenchido' }}
+    {{ $fornecedores[0]['telefone'] ?? 'Dado nao foi preenchido' }}) -
+    @switch($fornecedores[0]['ddd'])
+        @case('11')
+            Sao paulo - SP
+        @break
+
+        @case('32')
+            Juiz de fora - MG
+        @break
+
+        @case('85')
+            Fortaleza - CE
+        @break
+
+        @default
+            Estado nao identificado
+    @endswitch
 @endisset
