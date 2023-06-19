aeb9d8fc7d44977e9a7f1d1650e900eb77098270
Lincoln <jeanlincol@gmail.com>
2023-06-19 15:59:06 -0300

    Blade - Variavel Loop

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,8 +1,13 @@
 <h3>Fornecedor</h3>
 
+{{-- Variavel disponivel apenas no foreach e forelse --}}
+
 @isset($fornecedores)
     @forelse ($fornecedores as $fornecedor)
-        Fornecedor: @{{ $fornecedor['nome'] ?? 'Dado nao foi preenchido' }}
+        {{-- {{ dd($loop) }} --}}
+        Iteracao atual: {{ $loop->iteration }}
+        <br>
+        Fornecedor: {{ $fornecedor['nome'] ?? 'Dado nao foi preenchido' }}
         <br>
         Status: {{ $fornecedor['status'] ?? 'Dado nao foi preenchido' }}
         <br>
@@ -10,6 +15,15 @@
         <br>
         Telefone : ({{ $fornecedor['ddd'] ?? 'Dado nao foi preenchido' }}
         {{ $fornecedor['telefone'] ?? 'Dado nao foi preenchido' }})
+        <br>
+        @if ($loop->first)
+            Primeira iteracao do loop
+        @endif
+        @if ($loop->last)
+            Ultima iteracao do loop
+            <br>
+            Total de registros : {{ $loop->count }}
+        @endif
         <br>
         <hr>
     @empty Nao existem fornecedores cadastrados!
