87100872182bf5df22e144afd41ef7926106882f
Lincoln <jeanlincol@gmail.com>
2023-06-19 15:36:43 -0300

    Blade - Operador condicional de valor default (??)

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -12,7 +12,6 @@ class FornecedorController extends Controller
             0 => [
                 'nome' => 'Fornecedor 1',
                 'status' => 'N',
-                'cnpj' => '00.000.000/000-00'
             ],
             1 => [
                 'nome' => 'Fornecedor 2',
@@ -20,13 +19,6 @@ class FornecedorController extends Controller
                 'cnpj' => '00.000.000/000-00'
             ]
         ];
-
-        /*
-        condicao ? se verdade : se falso
-        condicao ? se verdade : (condicao ? se verdade : se falso)
-        */
-
-        echo isset($fornecedores[0]['cnpj']) ? 'CNPJ informado' : 'CNPJ nao informado';
 
         return view('app.fornecedor.index', compact('fornecedores'));
     }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -20,11 +20,10 @@
     <br>
     Status: {{ $fornecedores[0]['status'] }}
     <br>
-    @isset($fornecedores[0]['cnpj'])
-        CNPJ: {{ $fornecedores[0]['cnpj'] }}
-        @empty($fornecedores[0]['cnpj'])
-            - Vazio
-        @endempty
-    @endisset
-    <br>
+    CPNJ : {{ $fornecedores[0]['cnpj'] ?? 'Dado nao foi preenchido' }}
+    {{--
+        Se a variavel testada nao estiver definida (isset)
+    ou
+        $variavel testado possuir o valor null
+    --}}
 @endisset
