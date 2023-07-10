9a71b7c18864fac7ad8cd3fb5b603299f4da32cb
Jean Lincoln <jeanlincol@gmail.com>
2023-07-10 19:33:22 -0300

    Eloquent - Ajustando o nome da tabela no Model para um correto ORM

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -4,7 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/*
+ O laravel realiza uma automacao para encontrar a tabela no bando de dados,
+ Primeiro ele identifica o padrao camelcase no nome do model e para cada letra maiuscula,
+ ele coloca um separador underline "_", depois ele faz a transformacao de todas as letras para minusculo,
+ e por fim ele adiciona um "s" no final.
+ O problema neste model e que por motivo da ligua portuguesa (brasil), o correto seria "fornecedores", e nao "fornecedors".
+ Para isto podemos informar o parametro $table.
+*/
+
 class Fornecedor extends Model
 {
-    //
+    protected $table = 'fornecedores';
 }
