f44dae77be4b44107de2a0e46ed9900571410e94
Jean Lincoln <jeanlincol@gmail.com>
2023-07-13 19:46:57 -0300

    Selecionando registros com orWhere()

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -6,14 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 /*
 
-Selecionando registros com dois ou mais Wheres
+Selecionando registros utilizando a porta logica or
 
-select * from tabela where condicao1 and condicao2 and condicao 3
+select * from tabela where condicao1 or condicao2 or condicao 3
 
-SiteContato::where('campo_a_ser_comparado_por_igual','Conjunto de parametros')->where('campo_a_ser_comparado_por_igual','Conjunto de parametros')
+SiteContato::where('campo_a_ser_comparado_por_igual','Conjunto de parametros')->orWhere('campo_a_ser_comparado_por_igual','Conjunto de parametros')
+
 SiteContato::where('nome','<>','[3,6]')
-    ->whereIn('motivo_contato','[1,2]')+
-    ->whereBetween('created_at','['2020-08-01 00:00:00','2020-08-31 : 23:59:59']')
+    ->orWhereIn('motivo_contato','[1,2]')+
+    ->orWhereBetween('created_at','['2020-08-01 00:00:00','2020-08-31 : 23:59:59']')
     ->get();
 
 */
