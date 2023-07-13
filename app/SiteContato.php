c4274f0d28d7aa0191dd92d786344ea7d28f77b5
Jean Lincoln <jeanlincol@gmail.com>
2023-07-13 18:41:19 -0300

    Selecionando registros com whereBetween() e whereNotBetween()

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -6,20 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 /*
 
-Métodos staticos ::whereIn() e ::whereNotIn()
+Métodos staticos ::whereBetween() e ::whereNotBetween()
 
-::whereIn()
+Realiza uma comparação
 
-retorna um builder na qual realiza a consulta na tabela informada no primeiro parametro,
-de acordo com o conjuto de parametros informado no 2 parametro do método.
+::whereBetween()
 
-SiteContato::whereIn('campo_a_ser_comparado_por_igual','Conjunto de parametros')
-SiteContato::whereIn('motivo_contato','[1,3]')->get();
+retorna um builder na qual realiza uma comparação de numeros e datas baseada em um intervalo especificado no segundo parametro
 
-Na query acima ele iria retornar os usuarios que tiveram o motivo de contato 1 e 3.
+SiteContato::whereBetween('campo_a_ser_comparado_por_igual','Conjunto de parametros')
+SiteContato::whereBetween('motivo_contato','[3,6]')->get();
 
-o ::whereNotIn() é basicamente a mesma coisa, so que o contrario, ou seja a query acima seria o motivo de contato
-diferente de 1 e 3.
+Na query acima ele iria retornar os usuarios que tiveram o motivo de contato 3,4,5,6.
+
+::whereNotBetween()
+
+Basicamente a mesma coisa, so que o contrario, ou seja a query acima seria o motivo de contato
+diferente de menor que 3 e maior que 6.
 
 */
 
