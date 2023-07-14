bb1e92bccb9748590efdec65f1c00bfdee6c8a2e
Jean Lincoln <jeanlincol@gmail.com>
2023-07-13 21:44:30 -0300

    Eloquent - Atualizando registros (save)

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -4,6 +4,19 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/*
+
+o método save() atualiza os dados no banco de dados.
+
+$fornecedor = Fornecedor::find(1);
+
+$fornecedor->nome = 'Fornecedor 123'
+$fornecedor->site = 'fornecedor123.com.br'
+$fornecedor->email = 'fornecedor123@gmail.com'
+
+$contatos = SiteContato::all();
+
+*/
 class Fornecedor extends Model
 {
     protected $table = 'fornecedores';
--- app/SiteContato.php
+++ app/SiteContato.php
@@ -4,29 +4,6 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-/*
-
-O método pluck permite recuperar todos os valores de uma determinada chave,
-por exemplo de todos os dados de todos os usuariso gostariamos de retornar somente o email.
-
-$contatos = SiteContato::all();
-
-$contatos->pluck('email'); retornara um coleção somente com os emails
-$contatos->pluck('email')->first();
-$contatos->pluck('email')->last();
-$contatos->pluck('email')->reverse();
-$contatos->pluck('email')->toArray();
-$contatos->pluck('email')->toJson();
-
-podemos também com o segundo parametro inserir outra chave como indice, por exemplo,
-podemos colocar o nome como idice o email como chave.
-
-"Jorge" => "jorge@contato.com.br"
-
-$contatos->pluck('email', 'nome');
-
-*/
-
 class SiteContato extends Model
 {
 
