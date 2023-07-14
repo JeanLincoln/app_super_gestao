9007bd0c2eb8b7babdac2b13efc965bf8b80ccb6
Jean Lincoln <jeanlincol@gmail.com>
2023-07-13 21:04:01 -0300

    Eloquent - Ordenando registros

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -6,36 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 /*
 
-Podemos utilizar uma função de callback para que seja utilizados subgrupos de filtros, fazendo com que a consulta seja
-mais especifica como por exemplo:
+Podemos ordernar a consulta utilizando o orderBy
 
     select *
     from table
-    where nome = 'Jorger' or nome = 'ana' and (motivo_contato in (1,2) or id between 4 and 6)
-
-    repare que temos dois grupos acima  e não a query abaixo:
-
-    select *
-    from table
-    where nome = 'Jorger' or nome = 'ana' and motivo_contato in (1,2) or id between 4 and 6
+    order by desc
 
 Para conserguirmos reproduzir a query acima podemos fazer da seguinte forma
 
-SiteContato::where(function($query) {})->where(function($query) {})->orWhere(function($query) {})
+SiteContato::orderBy('nome') método ascedente
+SiteContato::orderBy('nome', 'desc') método descedente
 
-SiteContato::where(function($query) {
-    $query->where('nome','Jorge')
-        ->orWhere('nome','ana');
-})
-    ->where(function($query) {
-        $query->whereIn('motive_contato',[1,2])
-        ->orWhereBetween('id',[4,6]);
-    })
-    ->get();
+encadeando ordenações:
 
-
-
-
+SiteContato::orderBy('motivo_contato')->orderBy('nome', 'desc')
 
 */
 
