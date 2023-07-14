0810f69dc3aa8744a70d10476e6de5f6e79b7b8b
Jean Lincoln <jeanlincol@gmail.com>
2023-07-13 21:00:24 -0300

    Selecionando registros aplicando precedência em operações lógicas pt2

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -7,8 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 /*
 
 Podemos utilizar uma função de callback para que seja utilizados subgrupos de filtros, fazendo com que a consulta seja
-mais especifica.
+mais especifica como por exemplo:
 
+    select *
+    from table
+    where nome = 'Jorger' or nome = 'ana' and (motivo_contato in (1,2) or id between 4 and 6)
+
+    repare que temos dois grupos acima  e não a query abaixo:
+
+    select *
+    from table
+    where nome = 'Jorger' or nome = 'ana' and motivo_contato in (1,2) or id between 4 and 6
+
+Para conserguirmos reproduzir a query acima podemos fazer da seguinte forma
 
 SiteContato::where(function($query) {})->where(function($query) {})->orWhere(function($query) {})
 
