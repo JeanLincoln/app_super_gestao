9a6bb3e10d6e3ec101efc10335ae532aaab6e91f
Jean Lincoln <jeanlincol@gmail.com>
2023-07-15 15:11:52 -0300

    Validação de campos obrigatórios (required)

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -9,28 +9,19 @@ class ContatoController extends Controller
 {
     public function contato(Request $request)
     {
-        /*Há tres formas de registrar os dados no banco de dados*/
-
-        $contato = new SiteContato();
-
-        /* com o save()
-
-        $contato->nome = $request->input('nome');
-        $contato->telefone = $request->input('telefone');
-        $contato->email = $request->input('email');
-        $contato->motivo_contato = $request->input('motivo_contato');
-        $contato->mensagem = $request->input('mensagem');
-
-        $contato->save();
-        */
-
-        // com o fill e o request->all()
-        // $contato->fill($request->all());
-        // $contato->save();
-
-        // com o create e o request->all()
-        // $contato->create($request->all());
-
         return view('site.contato', ['titulo' => 'Contato (teste)']);
+    }
+
+    public function salvar(Request $request)
+    {
+        //realizar a validacao dos dados do request
+        $request->validate([
+            'nome' => 'required',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required',
+        ]);
+        // SiteContato::create($request->all());
     }
 }
