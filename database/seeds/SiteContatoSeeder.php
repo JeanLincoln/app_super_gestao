8c6e4d7110b823adc6d689d3db5716300d683b77
Jean Lincoln <jeanlincol@gmail.com>
2023-07-15 13:56:58 -0300

    factories

--- database/seeds/SiteContatoSeeder.php
+++ database/seeds/SiteContatoSeeder.php
@@ -12,12 +12,14 @@ class SiteContatoSeeder extends Seeder
      */
     public function run()
     {
-        $contato = new SiteContato();
-        $contato->nome = 'Sistemas SG';
-        $contato->telefone = '(11) 99999-8888';
-        $contato->email = 'contato@sg.com.br';
-        $contato->motivo_contato = 1;
-        $contato->mensagem = 'Seja bem-vind ao sistema Super Gestao';
-        $contato->save();
+        factory(SiteContato::class, 100)->create();
+
+        // $contato = new SiteContato();
+        // $contato->nome = 'Sistemas SG';
+        // $contato->telefone = '(11) 99999-8888';
+        // $contato->email = 'contato@sg.com.br';
+        // $contato->motivo_contato = 1;
+        // $contato->mensagem = 'Seja bem-vind ao sistema Super Gestao';
+        // $contato->save();
     }
 }
