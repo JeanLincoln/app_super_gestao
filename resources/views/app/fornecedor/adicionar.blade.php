1c86284a24d78ec14dcf6e04b19ca7d9d660f8ce
Jean Lincoln <jeanlincol@gmail.com>
2023-07-18 22:34:03 -0300

    Implementando o cadastro de fornecedores parte 2 (inclusão de registros)

--- resources/views/app/fornecedor/adicionar.blade.php
+++ resources/views/app/fornecedor/adicionar.blade.php
@@ -12,12 +12,19 @@
             </ul>
         </div>
         <div class="informacao-pagina">
+            {{ $msg }}
             <div style="width: 30%; margin-left:auto; margin-right:auto;">
-                <form action="POST">
-                    <input type="text" name="nome" placeholder="Nome" class="borda-preta">
-                    <input type="text" name="site" placeholder="Site" class="borda-preta">
-                    <input type="text" name="uf" placeholder="UF" class="borda-preta">
-                    <input type="text" name="email" placeholder="Email" class="borda-preta">
+                <form method="POST" action="{{ route('app.fornecedor.adicionar') }}">
+                    @csrf
+                    <input type="text" name="nome" placeholder="Nome" value="{{ old('nome') }}" class="borda-preta">
+                    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
+                    <input type="text" name="site" placeholder="Site" value="{{ old('site') }}" class="borda-preta">
+                    {{ $errors->has('site') ? $errors->first('site') : '' }}
+                    <input type="text" name="uf" placeholder="UF" value="{{ old('uf') }}" class="borda-preta">
+                    {{ $errors->has('uf') ? $errors->first('uf') : '' }}
+                    <input type="text" name="email" placeholder="Email" value="{{ old('email') }}"
+                        class="borda-preta">
+                    {{ $errors->has('email') ? $errors->first('email') : '' }}
                     <button type="submit" class="borda-preta">Cadastrar</button>
                 </form>
             </div>
