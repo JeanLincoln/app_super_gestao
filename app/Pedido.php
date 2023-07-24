8863001b2e282cc6d78f8fb6603336365d473107
Jean Lincoln <jeanlincol@gmail.com>
2023-07-24 15:24:42 -0300

    Implementando a tela de cadastro de produtos para um determinado pedido e o metodo belongsToMany

--- app/Pedido.php
+++ app/Pedido.php
@@ -8,6 +8,19 @@ class Pedido extends Model
 {
     protected $fillable = ['cliente_id'];
 
+    public function produtos()
+    {
+        // return $this->belongsToMany('App\Produto', 'pedidos_produtos');
+        return $this->belongsToMany('App\Item', 'pedidos_produtos', 'pedido_id', 'produto_id');
+
+        /*  PARÂMETROS
+            1 - Modelo do relacionamento NxN em relação o modelo que estamos implementando.
+            2 - Tabela auxiliar que armazena os registros de relacionamento.
+            3 - Representa o nome da FK da tabela mapeada pelo modelo na tabela de relacionamento.
+            4 - Representa o nome da FK da tabela mapeada pelo model utilizado no relacionamento que estamos implementando.
+        */
+    }
+
     public function cliente()
     {
         return $this->belongsTo('App\Cliente');
